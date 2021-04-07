98b092566aa45509fb60d199a8affaf70eeac163
Zach Ritter <zritter@boyne.com>
2021-04-07 18:01:01 -0400

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,52 @@
+using Imageflow.Server;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Imageflow.Server.HybridCache;
+using System.IO;
+
+namespace ImageOptimizer
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddImageflowHybridCache(new HybridCacheOptions("//Mac/Home/Desktop/Transfer/ImageflowCache")
+            {
+                CacheSizeLimitInBytes = (long)1 * 1024 *1024 * 1024 //1GB
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+
+            app.UseImageflow(new ImageflowMiddlewareOptions()
+                .MapPath("/", "//Mac/Home/Desktop/Transfer/Boyne Resorts Meetings XML")
+                .SetAllowCaching(true)
+                );
+        }
+    }
+}
